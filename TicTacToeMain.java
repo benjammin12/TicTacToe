e356ad9c709b032e13d27958ba747cf219f2e3cf
benjammin12 <benxerri@gmail.com>
2016-05-09 19:27:33 -0400

    Added main w/ comments

---
+++ TicTacToeMain.java
@@ -0,0 +1,12 @@
+// Author: Ben Xerri
+// Objective: TicTacToe game in java
+// You can use a non default constructor 
+// To create a board >= 3, otherwise you will get
+// Traditional 3x3 board, enjoy!
+
+public class TicTacToeMain {
+    public static void main(String[] args) {
+        TicTacToeGame game = new TicTacToeGame();
+        game.start();
+    }
+}
